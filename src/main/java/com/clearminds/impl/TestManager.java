25b0c8d028801ba4f4cf0235166216c991383e5d
Diana Zambrano <damari01zambrano@gmail.com>
2020-12-10 11:48:34 -0500

    Ejercicio 3 parte 2 propiedad

--- src/main/java/com/clearminds/impl/TestManager.java
+++ src/main/java/com/clearminds/impl/TestManager.java
@@ -1,5 +1,9 @@
 package com.clearminds.impl;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
 import com.clearminds.excepciones.InstanceException;
 import com.clearminds.model.Persona;
 
@@ -10,15 +14,37 @@ public class TestManager {
 		Persona p = new Persona("Alex", "Mena", 15);
 		String nombreClaseBDD = "com.clearminds.impl.ServicioPersonaBDD";
 		String nombreClaseArcvivo = "com.clearminds.impl.ServicioPersonaArchivos";
+		String nombre=leerPropiedad("clase");
 		PersonaManager pm;
 		try {
-			pm = new PersonaManager(nombreClaseArcvivo);
+			pm = new PersonaManager(nombre);
 			pm.insertarPersona(p);
 		} catch (InstanceException e) {
-			
+
 			e.printStackTrace();
 		}
 
+	}
+
+	public static String leerPropiedad(String nombrePropiedad) {
+		Properties p = new Properties();
+		String respuesta = null;
+
+		try {
+
+			p.load(new FileReader("clase.properties"));
+			if (p.getProperty(nombrePropiedad) != null) {
+				respuesta = p.getProperty(nombrePropiedad);
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return null;
+		}
+
+		return respuesta;
 
 	}
 
